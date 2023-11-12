fc06382a20be3fdf0d1ff461ebea05274030108d
goldegg1208 <79674569+goldegg1208@users.noreply.github.com>
2023-11-13 02:14:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -86,23 +86,30 @@ $(function(){
 
 $(function(){
 	var chart = $('.chart');
-
-	chart.each(function(){
-		var item = $(this);
-		var title = item.find('h2');
-		var targetNum = title.attr('data-num');
-		var circle = item.find('circle');
-
-		$({rate:0}).animate({rate:targetNum},
-			{
-				duration:1500,
-				progress:function(){
-					var now = this.rate;
-					var amount = 630 - (630*now/100);
-					title.text(Math.floor(now));
-					circle.css({strokeDashoffset:amount});
-				}
-		});
+	var chartOST = chart.offset().top - 800;
+	
+	$(window).scroll(function(){
+		var currentSCT = $(this).scrolltop();
+		if(currentSCT >= chartOST){
+			chart.each(function(){
+				var item = $(this);
+				var title = item.find('h2');
+				var targetNum = title.attr('data-num');
+				var circle = item.find('circle');
+		
+				$({rate:0}).animate({rate:targetNum},
+					{
+						duration:1500,
+						progress:function(){
+							var now = this.rate;
+							var amount = 630 - (630*now/100);
+							title.text(Math.floor(now));
+							circle.css({strokeDashoffset:amount});
+						}
+				});
+			});
+		}
 	});
+	
 });
 
