1b5391228279102aa978626d03512add5eb15d85
goldegg1208 <79674569+goldegg1208@users.noreply.github.com>
2023-11-13 02:02:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,22 @@ $(function(){
 	});		
 });
 
+$(function(){
+	var chart = $('.chart');
+
+	chart.each(function(){
+		var item = $(this);
+		var title = item.find('h2');
+		var targetNum = title.attr('data-num');
+
+		$({rate:0}).animate({rate:targetNum},
+			{
+				duration:1500,
+				progress:function(){
+					var now = this.rate;
+					title.text(Math.floor(now));
+				}
+		});
+	});
+});
+
